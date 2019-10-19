a6b53f77f382ecdb2f08a3b1c524dabb231941df
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-10-19 16:40:49 -0400

    Cargo clippy fixes
    Clippy really didnt like the if let Err(_)'s , so i replaced them with if .is_err()'s instead.

--- src/cmd/create.rs
+++ src/cmd/create.rs
@@ -5,17 +5,18 @@ use crate::{
 use std::fs::{create_dir_all, write};
 use std::path::Path;
 
+
 pub fn create(name: &str) -> Result<(), String> {
     let toml_path = format!("{}/{}.toml", name, INPUT_TOML);
     if Path::new(&toml_path).exists() {
         return Err(String::from("Package already exists"));
     }
 
-    if let Err(_) = create_dir_all(name) {
+    if create_dir_all(name).is_err() {
         return Err(format!("Could not create folder \"{}\"", name));
     }
 
-    if let Err(_) = write(
+    if write(
         &toml_path,
         format!(
             "[package]
@@ -30,22 +31,22 @@ std = \"https://github.com/adam-mcdaniel/tsar-std\"
 ",
             name
         ),
-    ) {
+    ).is_err() {
         return Err(format!("Could not write to file \"{}\"", toml_path));
     }
 
-    if let Err(_) = write(format!("{}/.gitignore", name), GITIGNORE) {
+    if write(format!("{}/.gitignore", name), GITIGNORE).is_err() {
         return Err(String::from("Could not write gitignore"));
     }
 
-    if let Err(_) = create_dir_all(format!("{}/{}", name, SOURCE_FOLDER)) {
+    if create_dir_all(format!("{}/{}", name, SOURCE_FOLDER)).is_err() {
         return Err(format!(
             "Could not make \"{}\" directory when creating package \"{}\"",
             SOURCE_FOLDER, name
         ));
     }
 
-    if let Err(_) = create_dir_all(format!("{}/{}", name, FOREIGN_FOLDER)) {
+    if create_dir_all(format!("{}/{}", name, FOREIGN_FOLDER)).is_err() {
         return Err(format!(
             "Could not make \"{}\" directory when creating package \"{}\"",
             FOREIGN_FOLDER, name
@@ -57,7 +58,7 @@ std = \"https://github.com/adam-mcdaniel/tsar-std\"
         name, SOURCE_FOLDER, MAIN_FILE_NAME, FILE_EXTENSION
     );
 
-    if let Err(_) = write(&main_file, "println(\"Hello, world!\")") {
+    if write(&main_file, "println(\"Hello, world!\")").is_err() {
         return Err(main_file);
     }
 
